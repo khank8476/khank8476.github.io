9a077dd5ec90502ff2e89398f3c5ee554a8ed3c0
Kamran Khan <43128049+khank8476@users.noreply.github.com>
2022-07-27 12:11:45 +0530

    Update main.js

--- countdown-master/main.js
+++ countdown-master/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
 	/* Countdown Logic */
     // Set the date we're counting down to
-    var countDownDate = new Date("1 Apr, 2222 12:00:00").getTime();
+    var countDownDate = new Date("1 Apr, 2029 12:00:00").getTime();
 
     // Update the count down every 1 second
     var x = setInterval(function() {
